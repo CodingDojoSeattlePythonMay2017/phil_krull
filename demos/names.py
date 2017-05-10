2564a7b050d362a425e0990e73e7e67f0d990b09
phil-krull <pkrull@codingdojo.com>
2017-05-10 16:17:27 -0700

    added names demo

---
+++ demos/names.py
@@ -0,0 +1,31 @@
+students = [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'},
+     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+]
+
+# for student in students:
+#     # print student.keys()
+#     print student['first_name'], student['last_name']
+
+users = {
+ 'Students': [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'},
+     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+  ],
+ 'Instructors': [
+     {'first_name' : 'Michael', 'last_name' : 'Choi'},
+     {'first_name' : 'Martin', 'last_name' : 'Puryear'}
+  ]
+ }
+
+for user in users:
+    count = 0
+    print user
+    # print users[user]
+    for student in users[user]:
+        count += 1
+        print '{} - {} {} - {}'.format(count, student['first_name'].upper(), student['last_name'].upper(), len(student['first_name'])+len(student['last_name']))
